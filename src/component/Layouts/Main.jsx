1e5a93d6deb42628cb212f4ea2f11e5cf20b2d79
easyno <dlwlsghgk124@naver.com>
2020-01-03 23:31:30 +0900

    modify import stop_watch_page, calendar_page value

--- src/component/Layouts/Main.jsx
+++ src/component/Layouts/Main.jsx
@@ -1,11 +1,14 @@
 import React, { memo } from "react";
 import StopWatch from "../Main/StopWatch.jsx";
-import { STOP_WATCH_PAGE } from "../App.jsx";
+import Calendar from "../Main/Calendar.tsx";
+import { STOP_WATCH_PAGE, CALENDAR_PAGE } from "../App.jsx";
 
 const getPage = page => {
 	switch (page) {
 		case STOP_WATCH_PAGE:
 			return <StopWatch />;
+		case CALENDAR_PAGE:
+			return <Calendar />;
 	}
 };
 
